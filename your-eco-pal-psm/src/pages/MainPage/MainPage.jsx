ac226a1bd402ab26128240455b0cabe5a611b92a
Adam592 <adam.jablonski010@gmail.com>
2025-04-12 18:06:26 +0200

    background color and main page style

--- your-eco-pal-psm/src/pages/MainPage/MainPage.jsx
+++ your-eco-pal-psm/src/pages/MainPage/MainPage.jsx
@@ -6,10 +6,17 @@ import BottomNav from './components/BottomNav';
 const MainPage = () => {
   const { activeComponent, setActiveComponent, renderComponent } = useActiveComponent();
 
+  const pageStyle = {
+      backgroundColor: '#e8f5e9',
+      minHeight: '100vh',
+      paddingTop: '2rem',
+      paddingBottom: '2rem'
+    };
+    
   return (
     <Container fluid className="d-flex flex-column min-vh-100 p-0">
       <Row className="flex-grow-1 m-0">
-        <Col className="p-3 bg-light">{renderComponent({ setActiveComponent })}</Col>
+        <Col className="p-3" style={pageStyle}>{renderComponent({ setActiveComponent })}</Col>
       </Row>
 
       <BottomNav
